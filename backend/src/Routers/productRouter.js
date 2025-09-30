6b6093e3c51bcbf8f2c818c93cf01d374d134be6
zakaria <himranezakaria2@gmail.com>
2025-09-30 15:09:12 +0100

    modified the upload images in productRouter

--- backend/src/Routers/productRouter.js
+++ backend/src/Routers/productRouter.js
@@ -2,6 +2,8 @@ import express from "express";
 import Product from "../Modules/product.js";
 import protect from "../Middlewares/protect.js";
 import authorizeRoles from "../Middlewares/authorizeRoles.js";
+import upload from "../Middlewares/multer.js";
+import cloudinary from '../config/cloudinary.js'
 
 const productRouter = express.Router();
 
@@ -27,15 +29,35 @@ productRouter.post(
   "/",
   protect,
   authorizeRoles(["admin"]),
+  upload.array("images", 5),
   async (req, res) => {
     try {
-      const { name, description, sizes, prices, category, stock, images } = req.body;
+      const { name, description, sizes, prices, category, stock } = req.body;
 
       if (!name || !prices) {
         return res
           .status(400)
           .json({ message: "Please fill the essential fields (name, prices)" });
       }
+
+      const uploadPromises = req.files.map((file) => {
+        return new Promise((resolve, reject) => {
+          const stream = cloudinary.uploader.upload_stream(
+            { folder: "Product" },
+            (error, results) => {
+              if(error){
+                reject(error)
+              }
+              else{
+                resolve(results.secure_url)
+              }
+            }
+          )
+          stream.end(file.buffer)
+        })
+      })
+
+      const imageURLs = await Promise.all(uploadPromises)
 
       const product = new Product({
         name,
@@ -44,7 +66,7 @@ productRouter.post(
         prices,
         category,
         stock,
-        images,
+        images: imageURLs,
       });
       await product.save();
 
@@ -62,13 +84,38 @@ productRouter.post(
 productRouter.put(
   "/:id",
   protect,
-  authorizeRoles(["admin"], async (req, res) => {
+  authorizeRoles(["admin"]),
+  upload.array("images", 5),
+  async (req, res) => {
     try {
-      const { name, description, sizes, prices, category, stock, images } = req.body;
+      const { name, description, sizes, prices, category, stock  } = req.body;
+      let imageURLs
+
+      if(req.files && req.files.length > 0){
+        const uploadPromises = req.files.map((file) => {
+          return new Promise((resolve, reject) => {
+            const stream = cloudinary.uploader.upload_stream(
+              { folder: "Product" },
+              (error, results) => {
+                if(error){
+                  reject(error)
+                }
+                else{
+                  resolve(results.secure_url)
+                }
+              }
+            )
+
+            stream.end(file.buffer)
+          })
+        })
+
+        imageURLs = await Promise.all(uploadPromises)
+      }
 
       const updatedProduct = await Product.findByIdAndUpdate(
         req.params.id,
-        { name, description, sizes, prices, category, stock, images },
+        { name, description, sizes, prices, category, stock, ...(imageURLs && { images: imageURLs }) },
         { new: true }
       );
 
@@ -76,15 +123,15 @@ productRouter.put(
         return res.status(404).json({ message: "Product Not Found" });
       }
 
-      res.status(200).json({ message: "Product Updated" });
+      res.status(200).json({ message: "Product Updated", updatedProduct });
     } catch (error) {
       res.status(500).json({ error: error.message })
     }
   })
-);
+;
 
 //delete a product (admin only)
-productRouter.delete('/:id', protect, authorizeRoles(['admin'], async (req, res) => {
+productRouter.delete('/:id', protect, authorizeRoles(['admin']), async (req, res) => {
   try {
     const deletedProduct = await Product.findByIdAndDelete(req.params.id)
 
@@ -92,10 +139,10 @@ productRouter.delete('/:id', protect, authorizeRoles(['admin'], async (req, res)
       return res.status(404).json({ message: 'Product Not Found' })
     }
 
-    res.status(200).json({ message: 'Product deleted' })
+    res.status(200).json({ message: 'Product deleted', deletedProduct })
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
-}))
+})
 
 export default productRouter;
